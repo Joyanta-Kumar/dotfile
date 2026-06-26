d67c200a0b17199ca16aff53b4b8b3c39552db2a
Joyanta Kumar <joyroy122004@gmail.com>
2026-06-26 23:24:29 +0600

    Everything works

---
+++ nvim/lua/plugins/neotree.lua
@@ -0,0 +1,34 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    opts = {
+        window = {
+            width = 30,
+            position = "right",
+            mappings = {
+                ["h"] = "close_node",
+            },
+        },
+
+        filesystem = {
+            follow_current_file = {
+                enbled = true,
+                leave_dirs_open = false,
+            },
+
+            filtered_items = {
+                visible = false,
+                hide_dotfiles = false,
+                hide_gitignored = true,
+            },
+        },
+
+        use_libuv_file_watcher = true
+    },
+}
